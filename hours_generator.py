15e9e04cd8fc63d0ed8d3c03e941038f56d4e29f
Ignacy <ignashir@gmail.com>
2025-03-23 13:52:37 +0100

    Fixed import and added flexible formatting

--- hours_generator.py
+++ hours_generator.py
@@ -5,7 +5,7 @@ from typing import Optional
 
 class HourGenerator:
     @staticmethod
-    def generate_hours(begin: int, end: int):
+    def generate_hours(begin: int, end: int, format = str):
         """Function generating hours (tailored to yield 2 hour variables that are some time apart)
         It is important that when you want to generate 2 columns with hours, you place them in such order (earlier hour, later hour)
         """
@@ -15,23 +15,24 @@ class HourGenerator:
         minutes = int((begin_h - hours) * 60)
         begin_time = time(hour=hours, minute=minutes)
         while True:
-            yield begin_time
+            yield begin_time.strftime(format)
             duration = norm(loc=45, scale=10).rvs(1)[0]
             end_time = timedelta(minutes=int(duration))
             begin_time = (datetime.combine(datetime.today(), begin_time) + end_time).time()
 
-    def __init__(self, /, reset_limit: Optional[int] = 2, begin: Optional[int] = 8, end: Optional[int] = 18):
+    def __init__(self, /, reset_limit: Optional[int] = 2, begin: Optional[int] = 8, end: Optional[int] = 18, format: Optional[str] = "%H:%M"):
         self.limit = reset_limit
         self.count = 0
         self.func = HourGenerator.generate_hours
         self.begin_arg = begin
         self.end_arg = end
-        self.func_gen = self.func(self.begin_arg, self.end_arg)
+        self.format = format
+        self.func_gen = self.func(self.begin_arg, self.end_arg, self.format)
 
     def __next__(self):
         if self.count == self.limit:
             self.count = 0
-            self.func_gen = self.func(self.begin_arg, self.end_arg)
+            self.func_gen = self.func(self.begin_arg, self.end_arg, self.format)
         self.count += 1
         return next(self.func_gen)
         
--- rule_book.py
+++ rule_book.py
@@ -7,14 +7,20 @@ from datetime import time, datetime, timedelta
 import re
 import warnings
 
-from .hours_generator import HourGenerator
+from hours_generator import HourGenerator
 
 class RuleBook:
     """Class that implements all rules for generation of values
     """
     def __init__(self, localization: str = "en_US"):
         fake = Faker(locale=localization)
-        self.hour_generator = HourGenerator(reset_limit=2, begin=8, end=18)
+
+        self.config = {
+            "date_format": "%Y-%m-%d",
+            "time_format": "%H:%M:%S"
+        }
+
+        self.hour_generator = HourGenerator(reset_limit=2, begin=8, end=18, format=self.config["date_format"])
         # TODO pomyslec jak zmienic to na cos ładniejszego
         # Each column name have a certain value presented by a tuple (value generator, index)
         # if index = -1 -> we want the whole output
@@ -34,8 +40,8 @@ class RuleBook:
         "gearbox": (lambda : fake.boolean(60), -1),
         "brand": (lambda : fake.random_element(elements = ["Toyota", "Suzuki", "Renault", "Ford", "Opel", "Skoda", "Kia", "Volkswagen"]), -1),
         "cartype": (lambda : fake.random_element(elements = ["car", "truck", "motorcycle"]), -1),
-        "inspectiondate": (lambda : fake.date_between(start_date="-1y", end_date="-1w").strftime("%d-%m-%Y"), -1),
-        "examdate": (lambda : fake.date_between(start_date="-3y", end_date="-1w").strftime("%d-%m-%Y"), -1),
+        "inspectiondate": (lambda : fake.date_between(start_date="-1y", end_date="-1w").strftime(self.config["date_format"]), -1),
+        "examdate": (lambda : fake.date_between(start_date="-3y", end_date="-1w").strftime(self.config["date_format"]), -1),
         "beginhour": (self.hour_generator, -1),
         "endhour": (self.hour_generator, -1),
         "result": (lambda : fake.boolean(70), -1),
@@ -43,12 +49,12 @@ class RuleBook:
         "type": (lambda : fake.boolean(55), -1),
         "examcomment": (lambda : fake.text(max_nb_chars=200), -1),
         "registrationnumber": (lambda : fake.bothify(text='??######', letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'), -1),
-        "dateofendofwork": (lambda : fake.date_between(start_date="+1w", end_date="+1y").strftime("%d-%m-%Y"), -1),
-        "dateofacceptance": (lambda : fake.date_between(start_date="-5y", end_date="-1w").strftime("%d-%m-%Y"), -1),
+        "dateofendofwork": (lambda : fake.date_between(start_date="+1w", end_date="+1y").strftime(self.config["date_format"]), -1),
+        "dateofacceptance": (lambda : fake.date_between(start_date="-5y", end_date="-1w").strftime(self.config["date_format"]), -1),
         "comments": (lambda : fake.text(20), -1),
         "examid": (lambda : fake.random_int(min=1000, max=9999), -1),
-        "reservationdate": (lambda : fake.date_between(start_date="-3w").strftime("%d-%m-%Y"), -1),
-        "reservationhour": (lambda : fake.time(pattern="%H:%M"), -1),
+        "reservationdate": (lambda : fake.date_between(start_date="-3w").strftime(self.config["date_format"]), -1),
+        "reservationhour": (lambda : fake.time(pattern=self.config["time_format"]), -1),
         "examtype": (lambda : fake.boolean(50), -1), 
         "assignedexaminerid": (fake.identity_card_number, -1)
     }   
